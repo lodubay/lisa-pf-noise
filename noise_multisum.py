f1a43e1de675b52e247b586ec1ec044b7ecc6909
Liam Dubay <liam.dubay@gmail.com>
2019-06-14 11:00:17 -0500

    created multirun analysis script

--- noise_multisum.py
+++ noise_multisum.py
@@ -1,6 +1,5 @@
 print('Importing libraries...')
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D # Registers the 3D projection
 import numpy as np
 from pymc3.stats import hpd
 import os
@@ -26,68 +25,33 @@ def psd_summarize(run_data, channel, alpha=0.9):
     summary_psd = np.hstack((frequencies, medians, credible_intervals))
     return summary_psd
 
-print('Importing data files...')
+print('Importing data files:')
 # The index of the channel we're interested in
 channel = 1
-# Run directories
+# Current directory
 top_dir = os.getcwd()
-run_dirs = sorted(glob.glob('data/run_k/run_k_*/'))[0:1] # only a few for testing purposes
+# List of the run directories. Only using a few for testing purposes
+run_dirs = sorted(glob.glob('data/run_k/run_k_*/'))[0:4] 
 
 # Pull PSD files from target run
-summaries = []
-times = []
+summaries = [] # List of summary PSDs, one for each run
+times = [] # List of GPS times corresponding to each run
 for run_dir in run_dirs:
-    run = int(run_dir[-11:-1])
-    times.append(run)
-    print('Importing ' + str(run) + '...')
+    time = int(run_dir[-11:-1]) # 10-digit GPS time
+    times.append(time)
+    print('\tImporting ' + str(time) + '...')
     os.chdir(top_dir + '/' + run_dir)
     # Grab the files with a single-digit index first to sort them correctly
     # Assumes file name format 'psd.dat.#' and 'psd.dat.##'
     psd_files = sorted(glob.glob('psd.dat.[0-9]'))
     psd_files += sorted(glob.glob('psd.dat.[0-9][0-9]'))
-    # Import PSD files
-    run_data = []
-    for psd_file in psd_files:
-    run_data.append(np.loadtxt(psd_file))
-# 3D array: (chain index, frequency index, channel)
-# If the number of rows differs from previous runs, adjust
-run_data = np.array(run_data)
-if run_dirs.index(run_dir) > 0:
-        difference = all_data[-1].shape[-2] - run_data.shape[-2]
-        # Create filler array assigned value 2, which will later be masked
-        # Filler array shape: (# of psds, difference between arrays, # columns in run_data)
-        filler_array = np.full((run_data.shape[0], np.abs(difference), run_data.shape[2]), 2)
-        # If it's smaller, add extra rows:
-        if difference > 0:
-            run_data = np.hstack((run_data, filler_array))
-        # If it's larger, add extra rows to all previous runs:
-        elif difference < 0:
-            all_data = [np.hstack((data, filler_array)) for data in all_data]                    
-    all_data.append(np.array(run_data))
-# 4D array: (run index, chain index, frequency index, channel)
-# Replace trailing 2s with empty cells
-all_data = np.ma.masked_equal(np.array(all_data), 2)
+    # Import PSD files into 3D array
+    run_data = np.array([np.loadtxt(psd_file) for psd_file in psd_files])
+    # Strip rows of 2s
+    run_data = run_data[:,np.min(run_data!=2., axis=(0,2))]
+    # Create 2D summary array and append to running list
+    summaries.append(psd_summarize(run_data, channel, alpha=0.9))
 
-# Set up figure
-print('Initializing figure...')
-fig1 = plt.figure(1)
-ax = fig1.add_subplot(111)
-# Axes limits
-all_freq = all_data[:,:,:,0].flatten()
-all_channel = all_data[:,:,:,channel].flatten()
-ax.set_xlim(np.min(all_freq), np.max(all_freq))
-ax.set_ylim(np.min(all_channel), np.max(all_channel))
-for run in range(len(all_data)):
-    plt.scatter(all_data[run,:,:,0], all_data[run,:,:,channel], marker='.', s=1)
-plt.show()
-
-# 3D figure
-print('Initializing 3D figure...')
-fig2 = plt.figure(2)
-ax = fig2.add_subplot(111, projection='3d')
-times_long = np.array(sorted(times * all_data.shape[1] * all_data.shape[2]))
-ax.scatter(all_data[:,:,:,0], times_long, all_data[:,:,:,channel], marker='.', s=1)
-ax.set_xlabel('Frequency')
-ax.set_ylabel('GPS time')
-ax.set_zlabel('Channel ' + str(channel) + ' noise')
-plt.show()
+# Turn into 3D array
+summaries = np.array(summaries)
+times = np.array(times)
