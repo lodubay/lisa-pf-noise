f471732808867b6da62683b515d55f83843dd637
reu <reu@1034lab1.nsstc.uah.edu>
2019-06-24 15:10:18 -0500

    modified psd to import all channels at once and save to one summary file

--- psd.py
+++ psd.py
@@ -5,38 +5,50 @@ import os
 import glob
 import time_functions as tf
     
-def import_channel(time_dir, channel):
+def import_time(time_dir):
     '''
     Import and combine all psd.dat files in a single time directory 
-    for one channel. Assumes file name format 'psd.dat.#' and 'psd.dat.##'.
+    for many channels. Assumes file name format 'psd.dat.#' and 'psd.dat.##'.
     Returns a DataFrame with frequency increasing down the rows and 
-    chain index increasing across the columns.
+    chain index increasing across the columns. The DataFrame is MultiIndexed,
+    with indices (highest level to lowest) channel, time, frequency.
 
     Input
     -----
-      time_dir : relative path to the directory
-      channel : string, channel header
+      time_dir : relative path to the time directory
     '''
-    print('\tImporting ' + time_dir[-11:-1] + '...')
+    time = int(time_dir[-11:-1])
+    print('\tImporting ' + str(time) + '...')
+    # Channel names
+    channels = ['a_x', 'a_y', 'a_z', 'theta_x', 'theta_y', 'theta_z']
     # Column names
-    cols = ['FREQ', 'a_x', 'a_y', 'a_z', 'theta_x', 'theta_y', 'theta_z']
+    cols = ['FREQ'] + channels
     # Sort so that (for example) psd.dat.2 is sorted after psd.dat.19
     psd_files = sorted(glob.glob(os.path.join(time_dir, 'psd.dat.[0-9]'))) + \
         sorted(glob.glob(os.path.join(time_dir, 'psd.dat.[0-9][0-9]')))
     # Import PSD files into DataFrame
-    time_data = pd.concat(
-        [ pd.read_csv(
-            psd, sep=' ', usecols=range(len(cols)), names=cols, index_col=0
-        )[channel] for psd in psd_files ],
-        axis=1,
-        ignore_index=True
-    )
-    # Round frequency index to deal with floating point accuracy
-    time_data.index = np.around(time_data.index.get_level_values('FREQ'), 5)
+    time_data = []
+    for pf in psd_files:
+        psd = pd.read_csv(
+            pf, sep=' ', usecols=range(len(cols)), names=cols, index_col=0
+        )
+        # Define MultiIndex
+        # Round frequency index to 5 decimals to deal with floating point issues
+        midx = pd.MultiIndex.from_product(
+            [channels, [time], np.around(psd.index.get_level_values('FREQ'), 5)], 
+            names=['CHANNEL', 'TIME', 'FREQ']
+        )
+        # Concatenate columns vertically
+        psd = pd.concat([psd[channel] for channel in channels])
+        # Assign MultiIndex
+        psd.index = midx
+        time_data.append(psd)
+    # Concatenate psd series horizontally
+    time_data = pd.concat(time_data, axis=1, ignore_index=True)
     # Strip rows of 2s
     return time_data[time_data.iloc[:,0] < 2]
     
-def summarize_psd(time_data, gps_time):
+def summarize_psd(time_dir):
     '''
     Returns a DataFrame with the median and credible intervals for one time.
     Credible intervals are calculated using
@@ -45,71 +57,49 @@ def summarize_psd(time_data, gps_time):
     
     Input
     -----
-      time_data : a DataFrame of all PSDs for a single time
-      channel : string, channel header
+      time_dir : relative path to the time directory
     '''
-    freqs = time_data.index
+    # Import time data
+    time_data = import_time(time_dir)
+    # Grab MultiIndex
+    midx = time_data.index
+    # Calculate HPDs
     time_data_np = time_data.to_numpy().T
     hpd_50 = hpd(time_data_np, alpha=0.5)
     hpd_90 = hpd(time_data_np, alpha=0.1)
+    # Return summary DataFrame
     return pd.DataFrame({
-        'TIME'      : gps_time,
         'MEDIAN'    : time_data.median(axis=1),
-        'CI_50_LO'  : pd.Series(hpd_50[:,0], index=freqs),
-        'CI_50_HI'  : pd.Series(hpd_50[:,1], index=freqs),
-        'CI_90_LO'  : pd.Series(hpd_90[:,0], index=freqs),
-        'CI_90_HI'  : pd.Series(hpd_90[:,1], index=freqs),
-    }, index=time_data.index)
+        'CI_50_LO'  : pd.Series(hpd_50[:,0], index=midx),
+        'CI_50_HI'  : pd.Series(hpd_50[:,1], index=midx),
+        'CI_90_LO'  : pd.Series(hpd_90[:,0], index=midx),
+        'CI_90_HI'  : pd.Series(hpd_90[:,1], index=midx),
+    }, index=midx)
     
-def summarize_run(run, channel):
+def save_summary(run):
     '''
     Returns a multi-index DataFrame of PSD summaries across multiple times 
-    from one run folder. The first index represents time and the 
-    second frequency.
+    from one run folder. The first index represents channel, the second GPS time
+    and the third frequency.
     
     Input
     -----
       run : string, name of the run directory
-      channel : string, channel header
     '''
     # Get list of time directories within run directory
     time_dirs = tf.get_time_dirs(run)
-    # For testing purposes
-    
     # Pull PSD files from target run
     print('Importing ' + run + '...')
     # Concatenate DataFrames of all times; takes a while
-    summaries = pd.concat([
-        summarize_psd(import_channel(d, channel), int(d[-11:-1])) 
-        for d in time_dirs
-    ])
-    # Set TIME and FREQ columns as indices
-    return summaries.set_index(['TIME', summaries.index])
-    
-def save_summary(run, ch_name):
-    '''
-    Calls summarize_run() writes output to a pickle file, and returns output.
-    
-    Input
-    -----
-      run : string, name of the run directory
-      ch_name : string, name of the channel of interest
-    '''
-    summaries = summarize_run(run, ch_name)
-    print('Writing to PSD summaries file...')
-    summaries.to_pickle(
-        os.path.join('summaries', run, 'summary.' + ch_name + '.pkl')
-    )
+    summaries = pd.concat([summarize_psd(d) for d in time_dirs])
+    # Output to file
+    summary_file = os.path.join('summaries', run, 'summary.pkl')
+    print('Writing to ' + summary_file + '...')
+    summaries.to_pickle(summary_file)
     return summaries
-    
-def save_all_channels(run, channels):
-    for channel in channels:
-        save_summary(run, channel)
 
-def load_summary(run, ch_name):
-    return pd.read_pickle(
-        os.path.join('summaries', run, 'summary.' + ch_name + '.pkl')
-    )
+def load_summary(run):
+    return pd.read_pickle(os.path.join('summaries', run, 'summary.pkl'))
 
 def get_time_slice(summary, gps_time):
     return summary.xs(gps_time)
