23cef4ac2d42944899b6e190c0b3d7ee998d8740
reu <reu@1034lab1.nsstc.uah.edu>
2019-07-01 13:17:30 -0500

    removed unncessary linechain functions

--- src/linechain.py
+++ src/linechain.py
@@ -17,34 +17,6 @@ def get_counts(lc_file):
     lc = pd.read_csv(lc_file, usecols=[0], header=None, squeeze=True, dtype=str)
     return pd.Series([int(row[0]) for row in lc])
 
-def gen_evidence_df(run, line_evidence_file, evidence_threshold=0.5):
-    '''
-    Returns a DataFrame with times as rows and channels as columns. A cell is
-    marked True if that time and column showed sufficient evidence for a
-    line, determined by the evidence threshold
-    '''
-    time_dirs = tf.get_time_dirs(run)
-    gps_times = tf.get_gps_times(run)
-    # Create empty DataFrame
-    df = pd.DataFrame(index=gps_times, columns=range(6))
-    for t, c in list(itertools.product(range(len(time_dirs)), range(6))):
-        # Update progress
-        sys.stdout.write('\r' + str(t*6 + c+1) + '/' + str(len(time_dirs)*6))
-        sys.stdout.flush()
-        # linechain file name
-        lc_file = os.path.join(
-            time_dirs[t], 'linechain_channel' + str(c) + '.dat'
-        )
-        # Calculate fraction of steps with non-zero line counts
-        counts = [count for count in get_counts(lc_file) if count > 0]
-        detection_ratio = len(counts) / len(lc)
-        # Record whether this file provides evidence for lines
-        df.loc[gps_times[t], c] = detection_ratio > evidence_threshold
-    sys.stdout.write('\n')
-    sys.stdout.flush()
-    df.to_csv(line_evidence_file, sep=' ')
-    return df
-
 def gen_model_df(run, model_file):
     '''
     Returns a DataFrame with times as rows and channels as columns. Cells
@@ -84,29 +56,6 @@ def get_lines(run, channel, model_file):
         df = gen_model_df(run, model_file)
     # Return list of times
     return df[df.iloc[:,channel] > 0].index
-
-def best_line_model(run, time, channel):
-    # Get time directory
-    time_index = tf.get_gps_times(run).index(time)
-    time_dir = tf.get_time_dirs(run)[time_index]
-    # File name
-    lc_file = os.path.join(
-        time_dir, 'linechain_channel' + str(channel) + '.dat'
-    )
-    # Import first column
-    counts = pd.Series(get_counts(lc_file))
-    # Runoff: eliminates the least likely model, then transfers those counts
-    # to the model with one fewer line (unless there is none).
-    # Tries to give a bit more balance to the less-likely models
-    while len(set(counts)) > 1:
-        # Find the least-likely model (the count which appears the least)
-        unlikely = min(set(counts), key=list(counts).count)
-        # Find the nearest larger model, if it exists
-        if unlikely + 1 in set(counts):
-            counts[counts == unlikely] = unlikely - 1
-        else:
-            counts = counts[counts != unlikely]
-    return max(counts)
 
 def get_line_params(run, time, channel, model):
     # Get time directory
