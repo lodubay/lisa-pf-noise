b7e30d9684d0c2fd0f8c74028743281a22f4c0d3
reu <reu@1034lab1.nsstc.uah.edu>
2019-07-26 16:16:48 -0500

    added fft auto peak finding

--- src/fft_peak.py
+++ src/fft_peak.py
@@ -13,7 +13,8 @@ channel = 'y'
 freq = [1e-2]
 
 # Analysis parameters
-bin_width = 20 # number of points on either side of peak to bin
+bin_width = 8 # number of points on either side of peak to bin
+f_step = 5e-6 # amount to step between each check
 f_min = 8.5e-5 # minimum estimated frequency of peak
 f_max = 9e-5 # maximum estimated frequency of peak
 
@@ -29,23 +30,52 @@ freq = psd.get_exact_freq(run.psd_summary, freq)
 rfftfreq, rfft = psd.fft(run, channel, freq, log)
 fft_psd = np.absolute(rfft)**2
 
-# Find background mean and variance
-b1 = fft_psd[rfftfreq < f_min]
-bin_width = min(bin_width, b1.shape[0]) # make sure start doesn't go below 0
-b1 = b1[-bin_width:]
-b2 = fft_psd[rfftfreq > f_max][:bin_width]
-b_mean = np.mean(np.concatenate([b1, b2]))
-b1_var = np.var(b1)
-b2_var = np.var(b2)
-b_var = np.mean([b1_var, b2_var])
-b_std = np.sqrt(b_var)
+# Number of peak bins to check
+n_bins = int((rfftfreq[-bin_width] - rfftfreq[bin_width]) / f_step)
+# Cycle through frequency space looking for peaks
+f_peaks = []
+peaks = []
+sigs = []
+for i in range(n_bins):
+    # Define minimum and maximum peak frequencies
+    f_min = rfftfreq[bin_width] + i * f_step
+    f_max = f_min + f_step
+    
+    # Find background mean and variance
+    b1 = fft_psd[rfftfreq < f_min]
+    bin_width = min(bin_width, b1.shape[0]) # make sure start doesn't go below 0
+    b1 = b1[-bin_width:]
+    b2 = fft_psd[rfftfreq > f_max][:bin_width]
+    b_mean = np.mean(np.concatenate([b1, b2]))
+    b1_var = np.var(b1)
+    b2_var = np.var(b2)
+    b_var = np.mean([b1_var, b2_var])
+    b_std = np.sqrt(b_var)
+    
+    # Find peak value
+    peak_psd = fft_psd[(rfftfreq > f_min) & (rfftfreq < f_max)]
+    peak_range = rfftfreq[(rfftfreq > f_min) & (rfftfreq < f_max)]
+    print(peak_range)
+    peak_val = np.max(peak_psd)
+    f_peak = peak_range[np.argmax(peak_psd)]
+    significance = (peak_val - b_mean) / b_std
+    print(f'{f_peak}: {peak_val}, significance {significance} sigma')
+    print(f'Background level: {b_mean}\n')
+    if significance >= 3:
+        f_peaks.append(f_peak)
+        peaks.append(peak_val)
+        sigs.append(significance)
 
-# Find peak value
-peak_val = np.max(fft_psd[(rfftfreq > f_min) & (rfftfreq < f_max)])
-significance = (peak_val - b_mean) / b_std
-print(significance)
+# Significance table
+sig_df = pd.DataFrame({'FREQ': f_peaks, 'POWER': peaks, 'SIG': sigs})
+print(sig_df)
 
 # Plot
 plt.plot(rfftfreq[1:], fft_psd[1:])
+plt.scatter(f_peaks, peaks, c='r', marker='.')
+plt.ylabel('PSD')
+plt.xlabel('Frequency (Hz)')
+plt.title(f'Bin width = {bin_width}')
 #plt.yscale('log')
 plt.show()
+
